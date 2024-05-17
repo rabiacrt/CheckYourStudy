3075cf3a58b5594e3aa4a905c370dbeb2e6193e1
rabiacrt <147415954+rabiacrt@users.noreply.github.com>
2024-05-17 17:52:38 +0300

    Check Your Study
    This project is designed for students preparing for university entrance exams. It calculates the daily number of questions that need to be solved based on their net scores in practice tests. Additionally, it provides recommendations for supplementary resources and suggests potential universities. The aim is to assist students who have limited access to guidance counselors.

---
+++ CheckYourStudy/src/checkyourstudy/CheckYourStudyInfo.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package checkyourstudy;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class CheckYourStudyInfo {
+    
+   private String adınız, soyadınız, sınıfınız;
+    
+    public CheckYourStudyInfo()
+    {
+        adınız = new String();
+        soyadınız = new String();
+        sınıfınız = new String();
+        
+    }
+
+    /**
+     * @return the adınız
+     */
+    public String getAdınız() {
+        return adınız;
+    }
+
+    /**
+     * @param adınız the adınız to set
+     */
+    public void setAdınız(String adınız) {
+        this.adınız = adınız;
+    }
+
+    /**
+     * @return the soyadınız
+     */
+    public String getSoyadınız() {
+        return soyadınız;
+    }
+
+    /**
+     * @param soyadınız the soyadınız to set
+     */
+    public void setSoyadınız(String soyadınız) {
+        this.soyadınız = soyadınız;
+    }
+
+    /**
+     * @return the sınıfınız
+     */
+    public String getSınıfınız() {
+        return sınıfınız;
+    }
+
+    /**
+     * @param sınıfınız the sınıfınız to set
+     */
+    public void setSınıfınız(String sınıfınız) {
+        this.sınıfınız = sınıfınız;
+    }
+}
